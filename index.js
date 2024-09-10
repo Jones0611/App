42dd260dffa0fcff965cd855a444f114884b237b
João Vitor Rodrigues <joaovr724@gmail.com>
2024-09-10 18:16:56 -0300

    cadastrar.meta

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
+
+let meta = {
+    value:'Tomar 3L de água por dia',
+    checked: false,
+}
+let metas = [ meta ]
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta: "})
+    
+    if(meta.length == 0)
+    {
+        console.log('A meta não pode ser vazia.')
+        return
+    }
+
+    metas.push(
+        { value: meta, checked: false}
+    )
+}
 
 const start = async () => {
+    
     while(true){
 
-        const opcao = await select({
+        const opcao = await select
+        ({
             message: "Menu >",
-            choices: [
-            {
-                name:"Cadastrar meta",
-                value: "Cadastrar"
-            },
-            {
-                name: "Listar metas",
-                value: "listar"
-            },
-            {
-                name:"Sair",
-                value: "sair"
-            }
-        ]
+            choices: 
+            [
+                {
+                    name:"Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name:"Sair",
+                    value: "sair"
+                }
+            ]
         })
 
         switch(opcao){
             case "cadastrar":
-                console.log("Vamos Cadastra")
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
                 console.log("Vamos listar")
@@ -32,6 +55,7 @@ const start = async () => {
                 console.log("Até a próxima")
                 return
 
+ 
         }
     }
 }
